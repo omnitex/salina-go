27cbef61a93937280414bd08a0b83e36b6ba6d24
omnitex <omnitex.git@gmail.com>
2026-08-03 11:16:28 +0200

    chore: scaffold Vite + React + TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <h1 className="p-4 text-xl font-bold">Šalina Go</h1>
+    </div>
+  );
+}
